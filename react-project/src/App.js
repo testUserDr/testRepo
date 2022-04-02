c057697150b859f0b8873d6332446b786337dac9
testUserDr <dsuboti1@gmail.com>
2022-04-02 23:14:36 +0200

    update app

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,13 +1,30 @@
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Person from "./Components/Person";
 
-function App() {
-  return (
-    <div className="App">
-      <Person age={22} name="John" />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 29 },
+      { name: "Stephanie", age: 26 },
+    ],
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working</p>
+        <button>Switch Name</button>
+        <Person
+          age={this.state.persons[0].age}
+          name={this.state.persons[0].name}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
